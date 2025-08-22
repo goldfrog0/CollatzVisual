b443e2f4e0fe8e70e08c1ed841f34809c61569ed
xavier <xraygarr@gmail.com>
2025-08-22 00:47:39 -0500

    completed list genration function

--- app/Main.hs
+++ app/Main.hs
@@ -8,11 +8,22 @@ collatzCheck n
   | otherwise = collatzCheck $ n `div` 2
 
 collatzList :: (Bool, [Integer]) -> (Bool, [Integer])
+collatzList (_, []) = (False, [])
+collatzList l@(True, a)
+  | head a == 1 = l
+  | otherwise = collatzList (False, a)
 collatzList (False, a)
   | n == 1 = (True, a)
-  | odd n  = collatzList (False, a ++ [3*n + 1])
-  | even n = collatzList (False, a ++ [n `div` 2])
-  where n = last a
+  | head a <= 0 = (False, a)
+  | odd n  = collatzList (False, 3*n + 1 : a)
+  | even n = collatzList (False, n `div` 2 : a)
+  where n = head a
+collatzList _ = error "not intended input"
+
+generateCollatzList :: Integer -> [Integer]
+generateCollatzList a = snd . collatzList $ (False, [a])
+
+
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
